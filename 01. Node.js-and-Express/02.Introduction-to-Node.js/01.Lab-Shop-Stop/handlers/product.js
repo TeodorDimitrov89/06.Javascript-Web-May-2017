a6e448f675554c19f4073709762db343c982e93c
TeodorDimitrov89 <braveheart1989@abv.bg>
2017-05-17 14:35:15 +0300

    Simple Shop Stop site

---
+++ 01. Node.js-and-Express/02.Introduction-to-Node.js/01.Lab-Shop-Stop/handlers/product.js
@@ -0,0 +1,56 @@
+const fs = require('fs')
+const url = require('url')
+const path = require('path')
+const database = require('../config/database')
+const query = require('querystring')
+let pathFile = path.normalize(path.join(__dirname, '../views/products/add.ejs'))
+module.exports = (req, res) => {
+  req.pathname = req.pathname || url.parse(req.url).pathname
+  let continueWithNextHandle = false
+  if (req.pathname === '/product/add' && req.method === 'GET') {
+    fs.readFile(pathFile, (err, data) => {
+      if (err) {
+        console.log(err)
+        res.writeHead(404, {'Content-Type': 'text/plain'})
+        res.write('Page not Found!')
+        res.end()
+        return
+      }
+      res.writeHead(200, {'Content-Type': 'text/html'})
+      res.write(data)
+      res.end()
+    })
+  } else if (req.pathname === '/product/add' && req.method === 'POST') {
+    let body = ''
+    req.on('data', (data) => {
+      body += data
+    })
+    req.on('end', () => {
+      let parsedBody = query.parse(body)
+      // let product = database.products
+      // product = {
+      //   name: parsedBody.name,
+      //   description: parsedBody.description,
+      //   price: parsedBody.price,
+      //   image: parsedBody.image
+      // }
+      database.products.add(parsedBody)
+      fs.readFile('./views/home/index.ejs', (err, data) => {
+        if (err) {
+          console.log(err)
+          res.writeHead(404)
+          res.write('404 not found!')
+          res.end()
+          return
+        }
+        res.writeHead(302, {
+          'Location': '/'
+        })
+        res.end()
+      })
+    })
+  } else {
+    continueWithNextHandle = true
+    return continueWithNextHandle
+  }
+}
