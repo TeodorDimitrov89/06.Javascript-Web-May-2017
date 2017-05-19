927828d76b5a40c0f2f322a8a38cdf882e69d85f
TeodorDimitrov89 <braveheart1989@abv.bg>
2017-05-19 14:06:00 +0300

    clear old files

--- 01. Node.js-and-Express/02.Introduction-to-Node.js/handlers/index.js
+++
@@ -1,4 +0,0 @@
-const homeHandler = require('../handlers/home')
-const products = require('../handlers/product')
-const staticFiles = require('../handlers/static-files')
-module.exports = [homeHandler, products, staticFiles]
